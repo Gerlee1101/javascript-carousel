8cec21d758314078751ef6ba87336ce2e515087a
25LP1083 25LP1083 <25LP1083@pinecones-iMac.local>
2025-10-24 13:18:30 +0800

    first commit

---
+++ carousel/index.js
@@ -0,0 +1,14 @@
+const prevBtn = document.querySelector(".previous");
+const nextBtn = document.querySelector(".next");
+const slides = document.querySelector(".slides");
+const images = document.querySelectorAll(".slide");
+let i = 0;
+
+prevBtn.addEventListener("click", () => {
+  i++;
+  slides.style.transform = `translateX(-${i * 410}px)`;
+});
+nextBtn.addEventListener("click", () => {
+  i--;
+  slides.style.transform = `translateX(-${i * 410}px)`;
+});
