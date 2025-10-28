b6b31c2833e2d868c48f091fd5388869f0f9d5f8
25LP1083 25LP1083 <25LP1083@pinecones-iMac.local>
2025-10-28 13:00:13 +0800

    whack-a-model

--- reaction time/index.js
+++ reaction time/index.js
@@ -1,5 +1,7 @@
 const sectionChange = document.querySelector(".section");
 const button = document.querySelector("button");
+const clickContinue = document.querySelector(".text-small");
+
 let startTime = null;
 let reactionTime = null;
 
@@ -9,12 +11,28 @@ button.addEventListener("click", () => {
   setTimeout(() => {
     sectionChange.style.backgroundColor = "green";
     sectionChange.innerHTML = `<div class="text">Click quickly as you can</div>`;
-    startTime = Date.now;
+    startTime = Date.now();
   }, (Math.floor(Math.random() * 3) + 1) * 1000);
 });
-sectionChange.addEventListener("clik", () => {
+
+sectionChange.addEventListener("click", () => {
   if (startTime === null) return;
   if (reactionTime !== null) return;
+  console.log(Date.now(), startTime);
   reactionTime = Date.now() - startTime;
-  sectionChange.innerHTML = reactionTime;
+  sectionChange.style.backgroundColor = "blue";
+  sectionChange.innerHTML = `
+    <div class="text"> 
+      <img src="./n7cnecfpglc9e5bmku03o93pg2.png" width="70" height="70" /> 
+      <p>Your Reaction Time</p>
+      <p>${reactionTime} ms</p>
+      <button class="text-small">Click to continue</button>
+    </div>
+  `;
+
+  const resetBtn = document.querySelector(".text-small");
+
+  resetBtn.addEventListener("click", () => {
+    sectionChange.innerHTML = ` `;
+  });
 });
